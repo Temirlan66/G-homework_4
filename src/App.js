fd6e3607a42c2d11438c7f91062c04712fd41e40
Temirlan <admin@Temirlans-MacBook-Pro.local>
2023-05-26 01:23:51 +0600

    hm4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      surname: "",
+      patronymic: "",
+      email: "",
+      phone: null,
+    };
+  }
+
+  nameHandleChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  surnameHandleChange = (e) => {
+    this.setState({
+      surname: e.target.value,
+    });
+  };
+
+  patronymicHandleChange = (e) => {
+    this.setState({
+      patronymic: e.target.value,
+    });
+  };
+
+  emailHandleChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  phoneHandleChange = (e) => {
+    this.setState({
+      phone: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      "Name:" +
+        this.state.name +
+        ", " +
+        "Surname:" +
+        this.state.surname +
+        ", " +
+        "Patronymic:" +
+        this.state.patronymic +
+        ", " +
+        "Email:" +
+        this.state.email +
+        ", " +
+        "PhoneNumber:" +
+        this.state.phone
+    );
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>SignUp</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Name"
+            type="text"
+            value={this.state.name}
+            onChange={this.nameHandleChange}
+          />
+
+          <input
+            placeholder="Surname"
+            type="text"
+            value={this.state.surname}
+            onChange={this.surnameHandleChange}
+          />
+
+          <input
+            placeholder="Patronymic"
+            type="text"
+            value={this.state.patronymic}
+            onChange={this.patronymicHandleChange}
+          />
+
+          <input
+            placeholder="Email"
+            type="email"
+            value={this.state.email}
+            onChange={this.emailHandleChange}
+          />
+
+          <input
+            placeholder="+996"
+            type="text"
+            value={this.state.phone}
+            onChange={this.phoneHandleChange}
+          />
+
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
